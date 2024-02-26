eeb6c74160d0bb9a29d12c95568a38d3e2d0cc3a
tylerb1 <tberb1290@gmail.com>
2024-02-25 22:06:27 -0600

    clean up

--- functions/summarize.js
+++ functions/summarize.js
@@ -1,59 +1,70 @@
 import fetch from 'isomorphic-fetch';
+import { createClient } from '@supabase/supabase-js';
+import 'dotenv';
+const {
+  VITE_PUBLIC_SUPABASE_URL,
+  SUPABASE_SERVICE_ROLE,
+} = process.env;
+const sb = createClient(VITE_PUBLIC_SUPABASE_URL, SUPABASE_SERVICE_ROLE);
 
 export async function handler(event) {
   let question = undefined;
   let answers = undefined;
+  const userId = JSON.parse(event.body).userId
   const jwt = event.headers.authorization.split(' ')[1]
   if (jwt) {
-    question = JSON.parse(event.body).question;
-    answers = JSON.parse(event.body).answers;
-  } else {
-    return {
-      statusCode: 401,
-      body: JSON.stringify({ error: 'Not Authorized (Invalid Token)' }),
+    const { data } = await sb.auth.getUser(jwt)
+    if (data.user.id === userId) {
+      question = JSON.parse(event.body).question;
+      answers = JSON.parse(event.body).joinedAnswers;
+    } else {
+      return {
+        statusCode: 401,
+        body: JSON.stringify({ error: 'Not Authorized (Invalid Token)' }),
+      };
+    }
+    const myPrompt = `Describe and summarize the following list of answers to the question "${question}" as accurately and concisely as possible: ${answers}`;
+    const openAiConfig = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'gpt-4',
+        messages: [
+          {
+            "role": "system",
+            "content": "You are a helpful assistant."
+          },
+          {
+            "role": "user",
+            "content": myPrompt
+          }
+        ],
+        temperature: 1,
+        max_tokens: 300,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+      }),
     };
-  }
-  const myPrompt = `Describe and summarize the following list of answers to the question "${question}" as accurately, comprehensively, and concisely as possible: ${answers}`;
-  const openAiConfig = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-    },
-    body: JSON.stringify({
-      model: 'gpt-4',
-      messages: [
-        {
-          "role": "system",
-          "content": "You are a helpful assistant."
-        },
-        {
-          "role": "user",
-          "content": myPrompt
-        }
-      ],
-      temperature: 1,
-      max_tokens: 200,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
-    }),
-  };
-  try {
-    const response = await fetch(
-      'https://api.openai.com/v1/chat/completions',
-      openAiConfig,
-    );
-    const data = await response.json();
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data?.choices?.[0].message),
-    };
-  } catch (error) {
-    console.error('Error:', error);
-    return {
-      statusCode: 500,
-      body: `error occured: ${error}`,
-    };
+
+    try {
+      const response = await fetch(
+        'https://api.openai.com/v1/chat/completions',
+        openAiConfig,
+      );
+      const data = await response.json();
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data?.choices?.[0].message),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    }
   }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import { onMount } from "svelte";
   import { sb } from "../supabase";
   import type { AuthSession } from '@supabase/supabase-js'
-  import { nanoid } from 'nanoid'
   import { 
     storePopup,
     getModalStore,
@@ -11,8 +10,9 @@
     initializeStores,
     type ToastSettings, 
     type ModalSettings,
-	Modal,
+	  Modal,
   } from '@skeletonlabs/skeleton';
+  import { nanoid } from 'nanoid'
   import { Copy, CheckCircled } from "radix-icons-svelte";
   import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 
@@ -22,21 +22,28 @@
   const toastStore = getToastStore();
 
   let isAdmin = false
+  let potentialEmail = ""
   let potentialRoomName = ""
   let potentialQuestion = ""
   let potentialAnswer = ""
   let roomId = ""
   let roomName = ""
   let currentQuestionIndex = 0
-  let currentQuestionAnswer = ""
   let questions: any[] = []
   let answers: { user: string, text: string }[] = []
   let joinedAnswers = ""
-  let loading = false
   let email = ""
   let userId = ""
   let session: AuthSession | null = null
-  $: currentQuestionUserAnswer = answers?.find((a: any) => a.user === userId)?.text || ""
+
+  $: currentQuestionUserAnswer = answers.find((a: any) => a.user === userId)?.text || ""
+  $: currentQuestionSummarizedAnswer = questions[currentQuestionIndex]?.generated_answer || ""
+  $: answers = questions[currentQuestionIndex]?.answers?.map((ans: any) => { 
+    return {
+      text: ans.answer_text,
+      user: ans.user_id,
+    }
+  }) || [];
 
   const showToast = (message: string) => {
     const t: ToastSettings = {
@@ -45,25 +52,34 @@
     toastStore.trigger(t);
   }
 
+  onMount(() => {
+    sb.auth.getSession().then(async ({ data }) => {
+      session = data.session
+      userId = session?.user?.id || ""
+      email = session?.user?.email || ""
+      isAdmin = session?.user.user_metadata.isAdmin
+      roomId = session?.user.user_metadata.currentRoomId || ""
+      if (roomId) {
+        await joinRoom(roomId)
+      }
+    })
+  })
+
   const handleLogin = async () => {
     try {
-      loading = true
       const { error } = await sb.auth.signInWithOtp({ 
-        email,
+        email: potentialEmail,
         options: {
           emailRedirectTo: import.meta.env.VITE_EMAIL_REDIRECT_URL
         }
       })
       if (error) {
-        showToast('Please use a valid email')
+        showToast('Please use a valid email.')
       } else {
-        showToast('Check your email for a link.')
+        showToast('Check your inbox for a login link.')
       }
-      email = ""
     } catch (error) {
-      showToast('Please use a valid email')
-    } finally {
-      loading = false
+      showToast('Please use a valid email.')
     }
   }
 
@@ -90,13 +106,18 @@
       .insert([{ name: roomName }])
       .select()
     potentialRoomName = ""
-    roomId = (data?.[0] as any).id
-    isAdmin = true
-    const { data: d2, error: e2 } = await sb.auth.updateUser({
-      data: { currentRoomId: roomId, isAdmin: true },
-    })
-    showToast('Room created.')
-    await listenToAnswers(roomId)
+    if (error) {
+      showToast('Error creating room. Please try again.')
+    } else {
+      roomId = data[0].id
+      isAdmin = true
+      await sb.auth.updateUser({
+        data: { currentRoomId: roomId, isAdmin: true },
+      })
+      showToast('Room created.')
+      await listenToNewAnswers(roomId)
+      await listenToQuestionUpdates(roomId)
+    }
   }
 
   const joinRoom = async (id: string) => {
@@ -117,101 +138,20 @@
         )
       `)
       .eq('id', id)
-    if (data?.length) {
-      roomId = data[0].id
+    if (error) {
+      showToast('Error joining room. Please try again.')
+    } else {
       roomName = data[0].name
       questions = data[0].questions
-      answers = questions[currentQuestionIndex]?.answers?.map((a: any) => { 
-        return {
-          text: a.answer_text,
-          user: a.user_id,
-        }
-      })
-      currentQuestionAnswer = questions[currentQuestionIndex]?.generated_answer || ""
-      const { data: d2, error: e2 } = await sb.auth.updateUser({
+      await sb.auth.updateUser({
         data: { currentRoomId: roomId },
       })
-      await listenToAnswers(roomId)
+      await listenToNewAnswers(roomId)
+      await listenToQuestionUpdates(roomId)
     }
   }
 
-  const submitQuestion = async () => {
-    const { data, error } = await sb
-      .from('questions')
-      .insert([{ 
-        question_text: potentialQuestion,
-        room_id: roomId,
-      }])
-      .select()
-    questions = [...questions, data?.[0]]
-    currentQuestionIndex = questions.length - 1
-    currentQuestionAnswer = questions[currentQuestionIndex].generated_answer || "";
-    answers = questions[currentQuestionIndex]?.answers?.map((a: any) => { 
-      return {
-        text: a.answer_text,
-        user: a.user_id,
-      }
-    });
-  }
-
-  const submitAnswer = async () => {
-    await sb
-      .from('answers')
-      .insert([{ 
-        answer_text: potentialAnswer,
-        question_id: questions[currentQuestionIndex].id,
-        room_id: roomId,
-        user_id: userId,
-      }])
-    answers = [...answers, { text: potentialAnswer, user: userId }]
-    potentialAnswer = ""
-    showToast("Answer submitted.")
-  }
-
-  const summarizeAnswers = async () => {
-    joinedAnswers = '\'' + answers.map((ans: any) => ans.text).join('\' \'') + '\''
-    try {
-      const resp = await fetch(
-        `${import.meta.env.VITE_EMAIL_REDIRECT_URL}/.netlify/functions/summarize`, 
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${session?.access_token}`,
-          },
-          body: JSON.stringify({ 
-            question: questions[currentQuestionIndex].question_text, 
-            answers: joinedAnswers
-          }),
-        }
-      )
-      const response = await resp.json();
-      const rawText = response?.content?.trim();
-      await sb
-        .from('questions')
-        .update({ generated_answer: rawText })
-        .eq('id', questions[currentQuestionIndex].id);
-      currentQuestionAnswer = rawText
-      showToast("Answers summarized.")
-    } catch (error) {
-      showToast('Error summarizing answers.')
-    }
-  }
-
-  onMount(() => {
-    sb.auth.getSession().then(async ({ data }) => {
-      userId = data.session?.user?.id || ""
-      email = data.session?.user?.email || ""
-      isAdmin = !!data.session?.user.user_metadata.isAdmin
-      roomId = data.session?.user.user_metadata.currentRoomId || ""
-      session = data.session
-      if (roomId) {
-        await joinRoom(roomId)
-      }
-    })
-  })
-
-  const listenToAnswers = async (roomId: string) => {
+  const listenToNewAnswers = async (roomId: string) => {
     await sb
       .channel(`answer-changes`)
       .on(
@@ -234,27 +174,108 @@
       .subscribe()
   }
 
+  const listenToQuestionUpdates = async (roomId: string) => {
+    await sb
+      .channel(`question-changes`)
+      .on(
+        'postgres_changes',
+        {
+          event: 'UPDATE',
+          schema: 'public',
+          table: 'questions',
+          filter: `room_id=eq.${roomId}`,
+        },
+        (msg) => {
+          questions = questions.map((q) => {
+            if (q.id === msg.new.id) {
+              return msg.new
+            }
+            return q
+          })
+        }
+      )
+      .subscribe()
+  }
+
   const joinRoomByName = async () => {
     const { data, error } = await sb
       .from('rooms')
       .select('id')
       .eq('name', potentialRoomName)
-    if (data?.length) {
-      await joinRoom((data[0] as any).id)
+    if (error) {
+      showToast('Error joining room. Please try again.')
     } else {
-      showToast('Room not found.')
+      roomId = data[0].id
+      await joinRoom(roomId)
     }
   }
 
-  const setQuestion = (i: number) => {
+  const setCurrentQuestion = (i: number) => {
     currentQuestionIndex = i;
-    currentQuestionAnswer = questions[i].generated_answer || "";
-    answers = questions[currentQuestionIndex]?.answers?.map((ans: any) => { 
-      return {
-        text: ans.answer_text,
-        user: ans.user_id,
+    currentQuestionSummarizedAnswer = questions[i].generated_answer || "";
+  }
+
+  const submitQuestion = async () => {
+    const { data, error } = await sb
+      .from('questions')
+      .insert([{ 
+        question_text: potentialQuestion,
+        room_id: roomId,
+      }])
+      .select()
+    if (error) {
+      showToast('Error submitting question. Please try again.')
+    } else if (data) {
+      questions = [...questions, data[0]]
+      currentQuestionIndex = questions.length - 1
+    }
+  }
+
+  const submitAnswer = async () => {
+    await sb
+      .from('answers')
+      .insert([{ 
+        answer_text: potentialAnswer,
+        question_id: questions[currentQuestionIndex].id,
+        room_id: roomId,
+        user_id: userId,
+      }])
+    potentialAnswer = ""
+    showToast("Answer submitted.")
+  }
+
+  const summarizeAnswers = async () => {
+    joinedAnswers = '\'' + answers.map((ans: any) => ans.text).join('\' \'') + '\''
+    try {
+      const resp = await fetch(
+        `${import.meta.env.VITE_EMAIL_REDIRECT_URL}/.netlify/functions/summarize`, 
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${session!.access_token}`,
+          },
+          body: JSON.stringify({ 
+            question: questions[currentQuestionIndex].question_text, 
+            joinedAnswers,
+            userId
+          }),
+        }
+      )
+      const response = await resp.json();
+      const rawText = response?.content?.trim();
+      if (rawText) {
+        await sb
+          .from('questions')
+          .update({ generated_answer: rawText })
+          .eq('id', questions[currentQuestionIndex].id);
+        showToast("Answers summarized.")
+      } else {
+        showToast('Error summarizing answers.')
       }
-    });
+    } catch (error) {
+      showToast('Error summarizing answers.')
+    }
   }
 </script>
 
@@ -268,19 +289,25 @@
 <Modal />
 <div class="grid md:grid-cols-[1fr_2fr_1fr] grid-cols-1 gap-4 p-4">
   <h1 class="justify-self-center md:col-start-2 text-2xl">Clave</h1>
+
   {#if userId}
     <div class="flex flex-col gap-2 max-w-48 md:col-start-3 col-start-1 text-right justify-self-end">
       <p class="text-xs">Logged in as: {email}</p>
+
       <button 
         type="button" 
         class="btn btn-sm variant-filled-surface rounded-md" 
         on:click={confirmSignout}
       >Sign Out</button>
     </div>
+
     <div class="flex flex-col gap-4 w-full md:col-start-2 col-start-1 justify-self-center">
       {#if roomId}
         <div class="flex flex-row">
-          <p class="text-sm text-center mr-2">Room code: <span id="room-code" class="font-bold text-sm">{roomName}</span></p>
+          <p class="text-sm text-center mr-2">
+            Room code: <span id="room-code" class="font-bold text-sm">{roomName}</span>
+          </p>
+
           <button on:click={() => {
             let roomCodeElement = document.getElementById('room-code');
             if (roomCodeElement) {
@@ -296,6 +323,7 @@
             <Copy className="h-2 w-2"/>
           </button>
         </div>
+
         <div class="flex flex-row gap-2">
           <input 
             class="input px-2 rounded-md"
@@ -303,6 +331,7 @@
             bind:value={potentialQuestion} 
             placeholder="New question" 
           />
+
           <button 
             class="btn btn-sm variant-filled-surface rounded-md"
             on:click={submitQuestion}
@@ -314,12 +343,14 @@
             class="btn btn-sm variant-filled-surface rounded-md" 
             on:click={async () => await createNewRoom()}
           >Create Room</button>
+
           <input 
             class="input px-2 rounded-md"
             type="text" 
             bind:value={potentialRoomName} 
             placeholder="Join an existing room..."
           />
+
           <button 
             class="btn btn-sm variant-filled-surface rounded-md" 
             on:click={joinRoomByName}
@@ -330,23 +361,33 @@
       {#if roomId && isAdmin && questions.length > 0}
         <div class="card p-2">
           <p><strong>{questions[currentQuestionIndex].question_text}</strong></p>
+
           <div class="flex flex-row gap-2 items-center mt-4 justify-between">
-            <span class="badge variant-filled">{questions[currentQuestionIndex]?.answers?.length || 0} answers</span>
+            <span class="badge variant-filled">
+              {questions[currentQuestionIndex]?.answers?.length || 0} answers
+            </span>
+
             <button 
               class="btn btn-sm variant-filled-surface rounded-md" 
               on:click={summarizeAnswers}
               disabled={(questions[currentQuestionIndex]?.answers?.length || 0) < 2}
             >Summarize</button>
           </div>
-          {#if currentQuestionAnswer}
-            <p class="mt-4 italic">{currentQuestionAnswer}</p>
+
+          {#if currentQuestionSummarizedAnswer}
+            <p class="mt-4 italic">
+              {currentQuestionSummarizedAnswer}
+            </p>
           {/if}
         </div>
       {:else if roomId && questions.length > 0}
         <div class="card flex flex-col gap-2 p-2">
           <p><strong>{questions[currentQuestionIndex].question_text}</strong></p>
+
           {#if answers.some((a) => a.user === userId)}
-            <p>Your answer: <span class="italic">{currentQuestionUserAnswer}</span></p>
+            <p>Your answer: <span class="italic">
+              {currentQuestionUserAnswer}
+            </span></p>
           {:else}
             <div class="flex flex-row gap-2">
               <input 
@@ -355,6 +396,7 @@
                 bind:value={potentialAnswer} 
                 placeholder="Answer" 
               />
+
               <button 
                 class="btn btn-sm variant-filled-surface rounded-md" 
                 on:click={submitAnswer}
@@ -362,7 +404,10 @@
               >Submit</button>
             </div>
           {/if}
-          <span class="badge variant-filled w-min mt-1">{questions[currentQuestionIndex]?.answers?.length || 0} answers</span>
+
+          <span class="badge variant-filled w-min mt-1">
+            {questions[currentQuestionIndex]?.answers?.length || 0} answers
+          </span>
         </div>
       {/if}
       {#each questions as q, i}
@@ -371,32 +416,38 @@
           <!-- svelte-ignore a11y-no-static-element-interactions -->
           <div 
             class="card card-hover p-2 flex flex-row gap-2 items-center cursor-pointer"
-            on:click={() => setQuestion(i)}
+            on:click={() => setCurrentQuestion(i)}
           >
             <p class="text-xs">{q.question_text}</p>
+
             {#if q.generated_answer}
               <CheckCircled class="min-h-6 min-w-6 ml-auto"/>
             {/if}
-            <span class="badge variant-filled {q.generated_answer ? '' : 'ml-auto'}">{q.answers?.length || 0}</span>
+
+            <span class="badge variant-filled {q.generated_answer ? '' : 'ml-auto'}">
+              {q.answers?.length || 0}
+            </span>
           </div>
         {/if}
       {/each}
     </div>
   {:else}
-    <form class="justify-self-center md:col-start-2" on:submit|preventDefault={handleLogin}>
+    <form 
+      class="justify-self-center md:col-start-2" 
+      on:submit|preventDefault={handleLogin}
+    >
       <div class="input-group input-group-divider grid-cols-[2fr_1fr] max-w-96 rounded-md">
         <input
-          id="email"
           class="input text-sm px-2 rounded-l-md rounded-tr-none rounded-br-none"
           type="email"
           placeholder="Email"
-          bind:value={email}
+          bind:value={potentialEmail}
         />
+
         <button 
           type="submit" 
           class="btn btn-md rounded-none rounded-r-md variant-filled-surface text-xs" 
           aria-live="polite" 
-          disabled={loading}
         >
           <span>Send login link</span>
         </button>
